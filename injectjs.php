1b299c8a57f762f0c52b253737618231a59a5d5c
Vibhav Sinha <v@vsinha.me>
2016-06-29 00:09:25 +0530

    initial commit

---
+++ injectjs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Injectjs
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: injectjs
+ * Domain Path: /languages
+ * @package Injectjs
+ */
